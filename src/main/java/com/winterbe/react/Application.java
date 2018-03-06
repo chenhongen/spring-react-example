aa88fec17ae1a7875a48da8a9ccc770a61f913ec
陈宏恩 <251601797@qq.com>
2018-03-06 17:51:30 +0800

    update springboot version to 1.5.x
    原项目down后无法运行，切换成新版本可运行

--- src/main/java/com/winterbe/react/Application.java
+++ src/main/java/com/winterbe/react/Application.java
@@ -3,7 +3,8 @@ package com.winterbe.react;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+//import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
